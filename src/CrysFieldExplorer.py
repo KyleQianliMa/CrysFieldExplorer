04cdb57354317c557caf90f925231c584023fa22
Kyle Qianli Ma <41385391+KyleQianliMa@users.noreply.github.com>
2023-11-04 04:58:59 -0400

    Add files via upload

--- src/CrysFieldExplorer.py
+++ src/CrysFieldExplorer.py
@@ -2,7 +2,14 @@ import numpy as np
 from numpy import sqrt
 import pandas as pd
 import pdb
+import matplotlib.pyplot as plt
 import Operators as op
+import sympy as sym
+from sympy import symbols, solve,cos,sin,Matrix, Inverse
+from scipy import optimize
+from scipy.special import wofz
+from scipy import integrate
+import scipy.linalg as LA
 
 class CrysFieldExplorer(op.Stevens_Operator,op.Quantum_Operator):
     '''
@@ -33,12 +40,11 @@ class CrysFieldExplorer(op.Stevens_Operator,op.Quantum_Operator):
         self.gamma=gamma
         self.Parameter=Parameter
         self.T=temperature
-        self.B=field
+        self.field=field
         super().__init__(Magnetic_ion)
 
     def Hamiltonian(self):
-        O=super().Stevens_hash(Stevens_idx)
-        
+        O=super().Stevens_hash(self.Stevens_idx) 
         H=0
         for i in O:
             if i[0] == '2':
@@ -112,6 +118,7 @@ class CrysFieldExplorer(op.Stevens_Operator,op.Quantum_Operator):
                 S.update({i:(s_degen[i]/total).item()})
             return S
         if Kramer == False:
+            s_degen=self.Intensity(gs)
             total=s_degen[N]
             S=dict()
             for i in s_degen:
@@ -121,56 +128,111 @@ class CrysFieldExplorer(op.Stevens_Operator,op.Quantum_Operator):
 
 class Utilities(CrysFieldExplorer):
     def __init__(self,Magnetic_ion,Stevens_idx,alpha,beta,gamma,Parameter,temperature,field):
-        super().__init__(Magnetic_ion)
+        super().__init__(Magnetic_ion,Stevens_idx,alpha,beta,gamma,Parameter,temperature,field)
         
-    # @staticmethod
-    # def lorentzian(x,Area,width,x0):
-    #     pi=np.pi
-    #     Y=(Area/pi)*(width/2)/((x-x0)**2+(width/2)**2)
-    #     return Y
+    @staticmethod
+    def lorentzian(x,Area,width,x0):
+        pi=np.pi
+        Y=(Area/pi)*(width/2)/((x-x0)**2+(width/2)**2)
+        return Y
     
-    # @staticmethod
-    # def chi(Obs,Exp):
-    #     summation=0
-    #     for i in range(len(Obs)):
-    #         if Exp[i]<2e-5:continue
-    #         else:
-    #             summation+=(Obs[i]-Exp[i])**2/Exp[i]
-    #     return summation
+    @staticmethod
+    def chi(Obs,Exp):
+        summation=0
+        for i in range(len(Obs)):
+            if Exp[i]<2e-5:continue
+            else:
+                summation+=(Obs[i]-Exp[i])**2/Exp[i]
+        return summation
     
-    # @staticmethod
-    # def test(a):
-    #     return print(a)
-    
-    # @staticmethod
-    # def susceptibility_VanVleck(Eigenvectors, Jx, Jy, Jz, E):
-    #       # S=3/2;L=6;J=15/2
-    #       # gj=(J*(J+1) - S*(S+1) + L*(L+1))/(2*J*(J+1)) +(J*(J+1) + S*(S+1) - L*(L+1))/(J*(J+1))
-    #       Na=6.0221409e23
-    #       muB=9.274009994e-21
-    #       kb=1.38064852e-16
-    #       C=(gj**2)*(Na)*(muB)**2/kb #X*Na* mu_b^2*X/kb in cgs unit
-    #       Z=0
-    #       # T=np.linspace(1, 300,150)
-    #       T=TX[0][::50]
+    def susceptibility_VanVleck(self):
+          ev,ef,H=super().Hamiltonian()  
+          Eigenvectors=ef
+          E=ev-ev[0]
+          S=self.S;L=self.L;J=self.J
+          gj=(J*(J+1) - S*(S+1) + L*(L+1))/(2*J*(J+1)) +(J*(J+1) + S*(S+1) - L*(L+1))/(J*(J+1))
+          Jx=super().Jx()
+          Jy=super().Jy()
+          Jz=super().Jz()
+          dim=int(2*self.J+1)
+          Na=6.0221409e23
+          muB=9.274009994e-21
+          kb=1.38064852e-16
+          C=(gj**2)*(Na)*(muB)**2/kb #X*Na* mu_b^2*X/kb in cgs unit
+          Z=0
+          T=np.linspace(1, 300,150)
+          # T=TX[0][::50]
          
-    #       for n in range(0,dim):
-    #           Z=Z+np.exp(-E[n]/T)
-    #       X=0
-    #       for n in range(0,dim):
-    #           for m in range(0,dim):
-    #               if np.abs(E[m]-E[n])<1e-5: X=X+(np.absolute(Eigenvectors[:,n].H*Jx*Eigenvectors[:,m]).item())**2*(np.exp(-E[n]/T))/T
-    #               else: X = X+ 2*(np.absolute(Eigenvectors[:,m].H*Jx*Eigenvectors[:,n]).item())**2*(np.exp(-E[n]/T))/(E[m]-E[n])
-    #       for n in range(0,dim):
-    #           for m in range(0,dim):
-    #               if  np.abs(E[m]-E[n])<1e-5: X=X+(np.absolute(Eigenvectors[:,n].H*Jy*Eigenvectors[:,m]).item())**2*(np.exp(-E[n]/T))/T
-    #               else: X = X+ 2*(np.absolute(Eigenvectors[:,m].H*Jy*Eigenvectors[:,n]).item())**2*(np.exp(-E[n]/T))/(E[m]-E[n])
-    #       for n in range(0,dim):
-    #           for m in range(0,dim):
-    #               if  np.abs(E[m]-E[n])<1e-5: X=X+(np.absolute(Eigenvectors[:,n].H*Jz*Eigenvectors[:,m]).item())**2*(np.exp(-E[n]/T))/T
-    #               else: X = X+ 2*((np.absolute(Eigenvectors[:,m].H*Jz*Eigenvectors[:,n]).item())**2)*(np.exp(-E[n]/T))/(E[m]-E[n])
-    #       X=C*X/(3*Z)
-    #       return T,X
+          for n in range(0,dim):
+              Z=Z+np.exp(-E[n]/T)
+          X=0
+          for n in range(0,dim):
+              for m in range(0,dim):
+                  if np.abs(E[m]-E[n])<1e-5: X=X+(np.absolute(Eigenvectors[:,n].H*Jx*Eigenvectors[:,m]).item())**2*(np.exp(-E[n]/T))/T
+                  else: X = X+ 2*(np.absolute(Eigenvectors[:,m].H*Jx*Eigenvectors[:,n]).item())**2*(np.exp(-E[n]/T))/(E[m]-E[n])
+          for n in range(0,dim):
+              for m in range(0,dim):
+                  if  np.abs(E[m]-E[n])<1e-5: X=X+(np.absolute(Eigenvectors[:,n].H*Jy*Eigenvectors[:,m]).item())**2*(np.exp(-E[n]/T))/T
+                  else: X = X+ 2*(np.absolute(Eigenvectors[:,m].H*Jy*Eigenvectors[:,n]).item())**2*(np.exp(-E[n]/T))/(E[m]-E[n])
+          for n in range(0,dim):
+              for m in range(0,dim):
+                  if  np.abs(E[m]-E[n])<1e-5: X=X+(np.absolute(Eigenvectors[:,n].H*Jz*Eigenvectors[:,m]).item())**2*(np.exp(-E[n]/T))/T
+                  else: X = X+ 2*((np.absolute(Eigenvectors[:,m].H*Jz*Eigenvectors[:,n]).item())**2)*(np.exp(-E[n]/T))/(E[m]-E[n])
+          X=C*X/(3*Z)
+          return T,X
+      
+    def gtensor(self):
+          Jx=super().Jx()
+          Jy=super().Jy()
+          Jz=super().Jz()
+          S=self.S;L=self.L;J=self.J
+          gj=(J*(J+1) - S*(S+1) + L*(L+1))/(2*J*(J+1)) +(J*(J+1) + S*(S+1) - L*(L+1))/(J*(J+1))
+          ev,ef,H=super().Hamiltonian()
+          minus=ef[:,1]
+          plus=ef[:,0]
+          Sx=[[(plus.H*Jx*plus).item(), (minus.H*Jx*plus).item()],
+              [(plus.H*Jx*minus).item(),(minus.H*Jx*minus).item()],
+              ]
+
+          Sy=[[(plus.H*Jy*plus).item(), (minus.H*Jy*plus).item()],
+              [(plus.H*Jy*minus).item(),(minus.H*Jy*minus).item()],
+              ]
+          Sz=[[(plus.H*Jz*plus).item(), (minus.H*Jz*plus).item()],
+              [(plus.H*Jz*minus).item(),(minus.H*Jz*minus).item()],
+              ]
+          g=[[Sx[1][0].real, Sx[1][0].imag, Sx[0][0].real],
+             [Sy[1][0].real, Sy[1][0].imag, Sy[0][0].real],
+             [Sz[1][0].real, Sz[1][0].imag, Sz[0][0].real],
+              ]
+          g=np.dot((2*gj),g)
+          return g
+    def gprime(self,axis):
+          g=self.gtensor()
+          theta=symbols('theta')
+          if axis == 'x':
+              A=Matrix([[1, 0, 0],
+                        [0,cos(theta),-sin(theta)],
+                        [0,sin(theta), cos(theta)]])
+          elif axis == 'y':
+              A=Matrix([[cos(theta),0,sin(theta)],
+                       [0,1,0],
+                       [-sin(theta),0,cos(theta)]])
+          elif axis == 'z':
+              A=Matrix([[cos(theta), -sin(theta), 0],
+                       [sin(theta),cos(theta),0],
+                       [0, 0, 1]])
+
+          gprime=g*A.inv()
+          theta=solve(gprime[0,2]-gprime[2,0])[0]
+          A=Matrix([[cos(theta),0,sin(theta)],
+                    [0,1,0],
+                    [-sin(theta),0,cos(theta)]])
+          gprime=g*A.inv()
+          gprime=np.array(gprime,dtype=float)
+          gxx=np.absolute(np.linalg.eigvals(gprime)[0])
+          gyy=np.absolute(np.linalg.eigvals(gprime)[2])
+          gzz=np.absolute(np.linalg.eigvals(gprime)[1])
+          return gxx,gyy,gzz, gprime
 #%% test
 if __name__ == "__main__":
     alpha=0.01*10.0*4/(45*35)
@@ -200,7 +262,8 @@ if __name__ == "__main__":
     Intensity=CEF.Neutron_Intensity(2, 0, True)
     
     uti=Utilities('Er3+', Stevens_idx, alpha, beta, gamma, Parameter, temp, field)
-    # uti.test()
-    
+    # T,X=uti.susceptibility_VanVleck()
+    uti.gprime('y')
+    # plt.plot(T,1/X)
 # B20,B21,B22,B40,B41,B42,B43,B44,B60,B61,B62,B63,B64,B65,B66
 
