fa4a7a80473dc79aea737d449859bb6804bfd709
Kyle Qianli Ma <41385391+KyleQianliMa@users.noreply.github.com>
2023-11-27 15:02:45 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='CrysFieldExplorer',
+    version='1.0.0',    
+    description='Fast optimization of CEF Hamiltonian',
+    url='https://github.com/KyleQianliMa/CrysFieldExplorer',
+    author='Kye Ma',
+    author_email='maq1@ornl.gov',
+    license='MIT License',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=['matplotlib',
+                      'scipy',
+                      'cma', 'numpy' ,
+                      'pyswarm'
+                      ],
+)
